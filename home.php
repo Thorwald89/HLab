57b9dea84fb2254d72e27f8f0d603d7a29a0ab88
Donato <donato.madalese@gmail.com>
2014-04-23 22:08:30 +0200

    modifica dell'indice

--- home.php
+++ home.php
@@ -309,7 +309,7 @@ Laboratorio di Manipolazione Cellulare
 </div>
 
 <div class="centro" >
-<iframe allowtransparency="true" width="700" height="400" src="version.php" id="centro" name="centro">
+<iframe allowtransparency="true" width="700" height="400" src="indice.php" id="centro" name="centro">
 </iframe>
 <center><a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Licenza Creative Commons" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">LabMan</span> di<a xmlns:cc="http://creativecommons.org/ns#" href="https://github.com/Thorwald89/LabMan" property="cc:attributionName" rel="cc:attributionURL">https://github.com/Thorwald89/LabMan</a> &eacute; distribuito con Licenza <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribuzione - Condividi allo stesso modo 4.0 Internazionale</a>.
 </center>
