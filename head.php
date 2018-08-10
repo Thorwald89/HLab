5c3e02afd91f23f18d3a18b1df25e5f5a9d05421
Donato <donato.madalese@gmail.com>
2018-08-10 14:06:00 +0200

    migrazione jquery mobile

---
+++ head.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+ * Copyright 2014 Thorwald Donato Madalese
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+include('setup/setup.php');
+	
+
+?>
+
+<!DOCTYPE html> 
+<html>
+<head>
+	<title><?= $titolo?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/git/jquery.mobile-git.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/git/jquery.mobile-git.js"></script>
